984173853f52c6ef3a8c68592d6b5c1fa9febd3f
danlin jia <dan.jia@jh-danlin.msl.lab>
2022-06-07 17:26:31 -0400

    multiple dirve access scripts

--- gdsio/gdsio_test.py
+++ gdsio/gdsio_test.py
@@ -1,12 +1,16 @@
 from ctl_exp import cmd_output, ctl_cmd
 import json
+import multiprocessing as mp
 
 io_strs = {0: "read", 1: "write", 2:"randread", 3:"randwrite"}
 
 def gdsio_parser(output_str):
     """
-    parse the output of gdsio
-    e.g., ""
+    parse the output of gdsio.
+    e.g., 
+    "IoType: READ XferType: GPUD Threads: 16 DataSetSize: 103993344/104857600(KiB) 
+    IOSize: 16384-32768-4096(KiB) Throughput: 6.168589 GiB/sec, Avg_Latency: 60200.233629 usecs 
+    ops: 4236 total_time 16.077547 secs"
     """
     word_list = output_str.split(" ")
     json_output = {}
@@ -21,62 +25,64 @@ def gdsio_parser(output_str):
         i += 1
     return json_output
 
-def gosio_wrapper(gpu, mount_point, io_mod, xfer_mod, w_list):
+def gdsio_wrapper(gpu, mount_point, thread, xfer, io, return_list=[]):
     """
-    execute gdsio command and save the output to json
-    io_mod, xfer_mod, w_list specify the combination of gdsio's 
-    parameters.
+    execute gdsio command and save the output to return_list.
     """
-    ret_list = []
+    exp_name = "{}_xfer_{}_io_{}_w_{}".format(io_strs[io], xfer, io, thread)
+    print("start {}".format(exp_name))
+    cmd = "/usr/local/cuda/gds/tools/gdsio -f {} -d {} -w {} -s 100G -x {} -i 16M:32M:4M -I {}".format(mount_point, gpu, thread, xfer, io)
+    cmd = cmd.split(" ")
+    cmd_obj = ctl_cmd(cmd)
+    cmd_obj.run_cmd()
+    output = cmd_obj.output
+
+    out_obj = cmd_output(gdsio_parser)
+    out_obj.parse_output(output)
+
+    json_obj = out_obj.json_output
+    json_obj["name"] = exp_name
+    json_obj["mount_point"] = mount_point
+    return_list.append(json_obj)
+    return return_list
+
+def union_test(gpu, mount_point):
+    w_list = [2]
+    xfer_mod = [0]
+    io_mod = [0, 1, 2, 3]
+    return_list = []
     for io in io_mod:
         for xfer in xfer_mod:
-            for w in w_list:
-                exp_name = "{}_xfer_{}_io_{}_w_{}".format(io_strs[io], xfer, io, w)
-                print("start {}".format(exp_name))
-                cmd = "./gdsio -f {} -d {} -w {} -s 100G -x {} -i 16M:32M:4M -I {}".format(mount_point, gpu, w, xfer, io)
-                cmd = cmd.split(" ")
-                cmd_obj = ctl_cmd(cmd)
-                cmd_obj.run_cmd()
-                output = cmd_obj.output
-
-                out_obj = cmd_output(gdsio_parser)
-                out_obj.parse_output(output)
-
-                json_obj = out_obj.json_output
-                json_obj["name"] = exp_name
-                ret_list.append(json_obj)
-    return ret_list
-
-def read_test(gpu, mount_point="/mnt/gds_test_mnt/test"):
-    w_list = [2, 4, 6, 8, 16, 32, 64, 128]
-    xfer_mod = [0, 1, 2, 3, 4, 5]
-    io_mod = [0, 2]
-    ret_list = gosio_wrapper(gpu, mount_point, io_mod, xfer_mod, w_list)
-
-    with open('read.json', 'w') as fp:
-        json.dump(ret_list, fp)
-
-def write_test(gpu, mount_point="/mnt/gds_test_mnt/test"):
-    w_list = [2, 4, 6, 8, 16, 32, 64, 128]
-    xfer_mod = [0, 1, 2, 3, 4, 5]
-    io_mod = [1, 3]
-    ret_list = gosio_wrapper(gpu, mount_point, io_mod, xfer_mod, w_list)
-
-    with open('write.json', 'w') as fp:
-        json.dump(ret_list, fp)
-
-def union_test(gpu, mount_point="/mnt/gds_test_mnt/test"):
-    w_list = [2, 4, 6, 8, 16, 32, 64, 128]
-    xfer_mod = [0, 1, 2, 3, 4, 5]
-    io_mod = [0, 1, 2, 3]
-    ret_list = gosio_wrapper(gpu, mount_point, io_mod, xfer_mod, w_list)
+            for thread in w_list:
+                return_list.extend(gdsio_wrapper(gpu, mount_point, thread, xfer, io))
 
     with open('{}.json'.format(gpu), 'w') as fp:
-        json.dump(ret_list, fp)
+        json.dump(return_list, fp)
 
-def one_gpu_multiple_drive(gpu, )
+def one_gpu_multiple_drive(gpu, files):
+    w_list = [16]
+    xfer_mod = [0, 4, 5]
+    io_mod = [0, 1, 2, 3]
+    ps_group = []
+    manager = mp.Manager()
+    return_list = manager.list()
+    for io in io_mod:
+        for xfer in xfer_mod:
+            for thread in w_list:
+                for mount_point in files:
+                    ps = mp.Process(target=gdsio_wrapper, args=(gpu, mount_point, thread, xfer, io, return_list))
+                    ps_group.append(ps)
+                    ps.start()
+                for ps in ps_group:
+                    ps.join()
+
+    with open('{}_{}drives.json'.format(gpu, len(files)), 'w') as fp:
+        json.dump(list(return_list), fp)
+
+    return list(return_list)
 
 if __name__ == "__main__":
-    # read_test(gpu=0)
-    # write_test(gpu=0)
-    union_test(gpu=0)
+    # union_test(gpu=0, mount_point="/mnt/nvme7/test")
+
+    files = [f"/mnt/nvme{i}/test" for i in [0, 1, 4, 5, 6, 7]]
+    return_list = one_gpu_multiple_drive(gpu=0, files=files)
--- gdsio/json_analyser.py
+++ gdsio/json_analyser.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import json
 import pandas as pd
+import numpy as np
 
 # with open('read.json', 'r') as fp:
 #     r_json = json.load(fp)
 
-write_df = pd.read_json('write.json', convert_axes=False)
-read_df = pd.read_json('read.json', convert_axes=False)
+# write_df = pd.read_json('write.json', convert_axes=False)
+# read_df = pd.read_json('read.json', convert_axes=False)
 
 
 # throughput/latency (Throughput/Avg_Latency)
 # across read/write (IoType/XferType/Threads)
 
-def plot_attri_across_workers(df, IoType, attri, title):
+def plot_attri(df, IoType, attri, title):
     # sequential_df = df[(df.IoType==" READ")|(df.IoType==" WRITE")]
     io_df = df[df.IoType==IoType]
     Xfertype = df.XferType.drop_duplicates().values
@@ -29,7 +30,43 @@ def plot_attri_across_workers(df, IoType, attri, title):
     plt.legend()
     plt.savefig("{}.png".format(title))
 
-plot_attri_across_workers(write_df, " RANDWRITE" ,"Throughput", "RANDWRITE_Throughput")
-plot_attri_across_workers(write_df, " WRITE" ,"Throughput", "WRITE_Throughput")
-plot_attri_across_workers(read_df, " READ" ,"Throughput", "READ_Throughput")
-plot_attri_across_workers(read_df, " RANDREAD" ,"Throughput", "RANDREAD_Throughput")
+def plot_attri(df, IoType, attri, x_label, title):
+    # sequential_df = df[(df.IoType==" READ")|(df.IoType==" WRITE")]
+    io_df = df[df.IoType==IoType]
+    Xfertype = df.XferType.drop_duplicates().values
+    fig, ax = plt.subplots()
+    for xfer in Xfertype:
+        xfer_df = io_df[io_df.XferType==xfer]
+        x = xfer_df[x_label].astype(str)
+        y = xfer_df[attri].apply(lambda x: x.split(" ")[1]).astype(float)
+        unit = xfer_df[attri].iloc[0].split(" ")[-1]
+        ax.plot(x, y, label=xfer,  marker=".")#, marker=".", color="g", linestyle='dashed')
+        ax.set_xlabel(x_label)
+        ax.set_ylabel("{} {}".format(attri, unit))
+        ax.set_title(title)
+    plt.grid()
+    plt.legend()
+    plt.savefig("{}.png".format(title))
+
+def plot_throughput_across_drives():
+    json_dfs =[]
+    for i in [1, 2, 4, 6]:
+        json_df = pd.read_json(f'0_{i}drives.json', convert_axes=False)
+        json_df["tpt"] = json_df.Throughput.apply(lambda x:x.split()[0]).astype(float)
+        drive_df = json_df.groupby(by=["XferType", "IoType", "Threads"], as_index=False).sum()
+        drive_df["Throughput"] = drive_df["tpt"].apply(lambda x: f" {x} GiB/s")
+        drive_df["drivers"] = np.array([i]*len(drive_df))
+        json_dfs.append(drive_df)
+
+    json_df = pd.concat(json_dfs)
+    plot_attri(json_df, " RANDWRITE" ,"Throughput", 'drivers' , "RANDWRITE_Throughput")
+    plot_attri(json_df, " WRITE" ,"Throughput", 'drivers' ,"WRITE_Throughput")
+    plot_attri(json_df, " READ" ,"Throughput", 'drivers' ,"READ_Throughput")
+    plot_attri(json_df, " RANDREAD" ,"Throughput", 'drivers' ,"RANDREAD_Throughput")
+
+# json_df = pd.read_json(f'0_{1}drives.json', convert_axes=False)
+
+# plot_attri(json_df, " RANDWRITE" ,"Throughput", 'Threads' , "RANDWRITE_Throughput")
+# plot_attri(json_df, " WRITE" ,"Throughput", 'Threads' ,"WRITE_Throughput")
+# plot_attri(json_df, " READ" ,"Throughput", 'Threads' ,"READ_Throughput")
+# plot_attri(json_df, " RANDREAD" ,"Throughput", 'Threads' ,"RANDREAD_Throughput")
